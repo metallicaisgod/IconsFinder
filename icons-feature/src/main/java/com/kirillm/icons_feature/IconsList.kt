1ed19f7ad610c9849cad7e64518401225d2ad58b
kmeshkov <metallicaisgod@yandex.ru>
2024-06-21 13:44:14 +0300

    ref

--- icons-feature/src/main/java/com/kirillm/icons_feature/IconsList.kt
+++ icons-feature/src/main/java/com/kirillm/icons_feature/IconsList.kt
@@ -1,5 +1,7 @@
 package com.kirillm.icons_feature
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
@@ -26,8 +29,13 @@ internal fun IconList(
 ) {
     val lazyListState = rememberLazyGridState()
 
-    Box(Modifier.fillMaxSize()) {
-        LazyVerticalGrid(columns = GridCells.Fixed(4), state = lazyListState) {
+    Box(Modifier.fillMaxSize().background(Color.White)) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(4),
+            state = lazyListState,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
 
             if (pagingItems.loadState.prepend is LoadState.Loading) {
                 item { ProgressIndicator() }
